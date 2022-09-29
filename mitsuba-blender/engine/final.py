8f259f9c24d7e18b978715dff586a9f3b5e93ab2
Arpit Agarwal <arpit15945@gmail.com>
2022-09-29 19:19:28 -0400

    main funcs to fill

--- mitsuba-blender/engine/final.py
+++ mitsuba-blender/engine/final.py
@@ -2,14 +2,38 @@ import bpy
 import tempfile
 import os
 import numpy as np
+import array
+import gpu
+from gpu_extras.presets import draw_texture_2d
 from ..io.exporter import SceneConverter
 
+class CustomDrawData:
+    def __init__(self, dimensions):
+        # Generate dummy float image buffer
+        self.dimensions = dimensions
+        width, height = dimensions
+
+        pixels = width * height * array.array('f', [0.1, 0.2, 0.1, 1.0])
+        pixels = gpu.types.Buffer('FLOAT', width * height * 4, pixels)
+
+        # Generate texture
+        self.texture = gpu.types.GPUTexture((width, height), format='RGBA16F', data=pixels)
+
+        # Note: This is just a didactic example.
+        # In this case it would be more convenient to fill the texture with:
+        # self.texture.clear('FLOAT', value=[0.1, 0.2, 0.1, 1.0])
+
+    def __del__(self):
+        del self.texture
+
+    def draw(self):
+        draw_texture_2d(self.texture, (0, 0), self.texture.width, self.texture.height)
 
 class MitsubaRenderEngine(bpy.types.RenderEngine):
 
     bl_idname = "MITSUBA"
     bl_label = "Mitsuba"
-    bl_use_preview = False
+    bl_use_preview = True
 
     # Init is called whenever a new render engine instance is created. Multiple
     # instances may exist at the same time, for example for a viewport and final
@@ -24,9 +48,75 @@ class MitsubaRenderEngine(bpy.types.RenderEngine):
     def __del__(self):
         pass
 
+    # For viewport renders, this method gets called once at the start and
+    # whenever the scene or 3D viewport changes. This method is where data
+    # should be read from Blender in the same thread. Typically a render
+    # thread will be started to do the work while keeping Blender responsive.
+    def view_update(self, context, depsgraph):
+        region = context.region
+        view3d = context.space_data
+        scene = depsgraph.scene
+
+        # Get viewport dimensions
+        dimensions = region.width, region.height
+
+        if not self.scene_data:
+            # First time initialization
+            self.scene_data = []
+            first_time = True
+
+            # Loop over all datablocks used in the scene.
+            for datablock in depsgraph.ids:
+                pass
+        else:
+            first_time = False
+
+            # Test which datablocks changed
+            for update in depsgraph.updates:
+                print("Datablock updated: ", update.id.name)
+
+            # Test if any material was added, removed or changed.
+            if depsgraph.id_type_updated('MATERIAL'):
+                print("Materials updated")
+
+        # Loop over all object instances in the scene.
+        if first_time or depsgraph.id_type_updated('OBJECT'):
+            for instance in depsgraph.object_instances:
+                pass
+
+    # For viewport renders, this method is called whenever Blender redraws
+    # the 3D viewport. The renderer is expected to quickly draw the render
+    # with OpenGL, and not perform other expensive work.
+    # Blender will draw overlays for selection and editing on top of the
+    # rendered image automatically.
+    def view_draw(self, context, depsgraph):
+        region = context.region
+        scene = depsgraph.scene
+
+        # Get viewport dimensions
+        dimensions = region.width, region.height
+
+        # Bind shader that converts from scene linear to display space,
+        gpu.state.blend_set('ALPHA_PREMULT')
+        self.bind_display_space_shader(scene)
+
+        if not self.draw_data or self.draw_data.dimensions != dimensions:
+            self.draw_data = CustomDrawData(dimensions)
+
+        self.draw_data.draw()
+
+        self.unbind_display_space_shader()
+        gpu.state.blend_set('NONE')
+
+    def render(self, depsgraph):
+        if self.is_preview:            
+            # self.render_preview(depsgraph)
+            pass
+        else:
+            self.render_final(depsgraph)
     # This is the method called by Blender for both final renders (F12) and
     # small preview for materials, world and lights.
-    def render(self, depsgraph):
+    def render_final(self, depsgraph):
         from mitsuba import set_variant
         b_scene = depsgraph.scene
         set_variant(b_scene.mitsuba.variant)
