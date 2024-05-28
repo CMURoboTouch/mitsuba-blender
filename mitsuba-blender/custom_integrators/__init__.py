23c326ed8a09f9c782fc6e61c2cb9a2bb1bd56b3
Arpit Agarwal <arpit15945@gmail.com>
2024-05-28 11:35:40 -0400

    Remove custom direct integrator

--- mitsuba-blender/custom_integrators/__init__.py
+++ mitsuba-blender/custom_integrators/__init__.py
@@ -1,7 +1,5 @@
-from .direct import MyDirectIntegrator
 from .pssmlt import MyPathIntegrator
 
 def register():
   from mitsuba import register_integrator
-  register_integrator("mydirect", lambda props: MyDirectIntegrator(props))
   register_integrator("pssmlt", lambda props: MyPathIntegrator(props))
